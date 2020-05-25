359f8482d1fcec984887fffba70c5462cabbad2f
Ostreszko <pawel.horaczy94@gmail.com>
2020-05-25 10:14:28 +0200

    Asteroidzz rotate

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    Rigidbody2D rb;
+    System.Random rand = new System.Random();
+    float rotateSpeed = 0f;
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        rotateSpeed = rand.Next(-100, 100) / 20f;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.rotation += rotateSpeed;
+    }
+
+}
